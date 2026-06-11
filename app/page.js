7b45dd1746e0af3197e2d5fae7f3700b96bc8489
Dagim <dagisis1224@gmail.com>
2026-06-11 11:33:34 +0100

    feat: add App Promo for Merkato Store

--- app/page.js
+++ app/page.js
@@ -46,7 +46,7 @@ export default function Home() {
       <WhyChoose />
       <Brands />
       <Testimonials />
-      {/* <AppPromo /> */}
+      <AppPromo />
       {/* <Newsletter /> */}
     </div>
   );
